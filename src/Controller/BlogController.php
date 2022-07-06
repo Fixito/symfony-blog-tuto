9b407175572eac542b777a97c5ea026e981f9c16
Thomas ROBERT <thomas_3004@hotmail.fr>
2022-07-06 16:14:19 +0200

    created routes in BlogController

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,9 +8,33 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-  #[Route('/blog', name: 'app_blog')]
+  #[Route('/', name: 'homepage')]
   public function index(): Response
   {
-    return new Response("<h1>Hello World!</h1>");
+    return $this->render("blog/index.html.twig");
+  }
+
+  #[Route('/add', name: 'article_add')]
+  public function add(): Response
+  {
+    return new Response("<h1>Ajouter un Article</h1>");
+  }
+
+  #[Route('/show/{url}', name: 'article_show')]
+  public function show($url): Response
+  {
+    return new Response("<h1>Lire l'Article $url</h1>");
+  }
+
+  #[Route('/edit/{id}', name: 'article_edit')]
+  public function edit($id): Response
+  {
+    return new Response("<h1>Modifier l'Article $id</h1>");
+  }
+
+  #[Route('/remove/{id}', name: 'article_remove')]
+  public function remove($id): Response
+  {
+    return new Response("<h1>Supprimer l'Article $id</h1>");
   }
 }
