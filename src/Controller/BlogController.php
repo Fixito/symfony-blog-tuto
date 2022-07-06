76fe557e8064100c0e76e7b22c89a053f21335d9
Thomas ROBERT <thomas_3004@hotmail.fr>
2022-07-06 20:07:39 +0200

    added assets/css and html in views

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -17,19 +17,23 @@ class BlogController extends AbstractController
   #[Route('/add', name: 'article_add')]
   public function add(): Response
   {
-    return new Response("<h1>Ajouter un Article</h1>");
+    return $this->render("blog/add.html.twig");
   }
 
   #[Route('/show/{url}', name: 'article_show')]
   public function show($url): Response
   {
-    return new Response("<h1>Lire l'Article $url</h1>");
+    return $this->render("blog/show.html.twig", [
+      "slug" => $url
+    ]);
   }
 
   #[Route('/edit/{id}', name: 'article_edit')]
   public function edit($id): Response
   {
-    return new Response("<h1>Modifier l'Article $id</h1>");
+    return $this->render("blog/edit.html.twig", [
+      "id" => $id
+    ]);
   }
 
   #[Route('/remove/{id}', name: 'article_remove')]
