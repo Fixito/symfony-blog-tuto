abff246e571250fd67234ce9cf0559b705b3275c
Fixito <thomas_3004@hotmail.fr>
2022-07-11 16:28:54 +0200

    added article and categories entities + add form

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use App\Entity\Article;
+use App\Form\ArticleType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
@@ -15,9 +20,26 @@ class BlogController extends AbstractController
   }
 
   #[Route('/add', name: 'article_add')]
-  public function add(): Response
+  public function add(Request $request, ManagerRegistry $doctrine): Response
   {
-    return $this->render("blog/add.html.twig");
+    $article = new Article();
+    $form = $this->createForm(ArticleType::class, $article);
+
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $article->setLastUpdateDate(new \DateTime());
+
+      if ($article->getIsPublished()) {
+        $article->setPublicationDate(new \DateTime());
+      }
+
+      // on récupère l'entity manager
+      $em = $doctrine->getManager();
+      $em->persist($article); // On confie notre entité à l'entity manager (on persiste l'entité)
+      $em->flush(); // On exécute la requête
+
+      return new Response('L\'article a bien été enregistrer.');
+    }
   }
 
   #[Route('/show/{url}', name: 'article_show')]
